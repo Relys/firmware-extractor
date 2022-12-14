6fb0d1eaa2076e6ce51e01dd2876480bd1b54196
Aaron <aaron@exphat.com>
2022-12-13 20:40:17 -0500

    Fill in actual function addresses

--- firmware.c
+++ firmware.c
@@ -1,14 +1,14 @@
-#define KEY_LOCATION 0x0d54
+#define KEY_LOCATION 0x08000d54
 #define BYTES_TO_READ 0x4
 
 // Our custom magic headers to show we're sending the firmware data
 #define HEADER_KEY_START 0x00
 #define HEADER_KEY_END 0x00
 
-#define unlock_memory  ((void (*)(void))0x00000000)
-#define set_lightbar   ((void (*)(unsigned int, unsigned int, unsigned int))0x00000000)
-#define read_bt_cmd    ((unsigned int (*)(void))0x00000000)
-#define send_bt_cmd    ((void (*)(unsigned int))0x00000000)
+#define unlock_memory   ((void (*)(void))0x08001092)
+#define set_lightbar    ((void (*)(unsigned int, unsigned int, unsigned int))0x08000c46)
+#define read_bt_byte    ((unsigned int (*)(void))0x00000000)
+#define send_bt_byte    ((void (*)(unsigned int))0x08001194)
 
 #include <string.h>
 
@@ -17,28 +17,28 @@ unsigned int last_cmd = 0x00;
 
 void main()
 {
-    set_lightbar(0, 0, 0); // Show that we've successfully run the firmware
+    set_lightbar(0xff, 0xbe, 0x00); // Show that we've successfully run the firmware
 
     while (last_cmd != HEADER_KEY_START) {
-        last_cmd = read_bt_cmd();
+        last_cmd = read_bt_byte();
     }
 
-    set_lightbar(0, 0, 0); // Acknowledge that we've received the start header
+    set_lightbar(0xbe, 0x00, 0xff); // Acknowledge that we've received the start header
 
     unlock_memory();
 
-    set_lightbar(0, 0, 0); // Show that we've unlocked the flash and are going to read
+    set_lightbar(0xff, 0x00, 0xbe); // Show that we've unlocked the flash and are going to read
 
     for (int i = 0; i < BYTES_TO_READ; i++) {
         memcpy(&keys[i], (unsigned int*)(KEY_LOCATION + (i * sizeof(unsigned int))), sizeof(unsigned int));
     }
 
     for (int i = 0; i < BYTES_TO_READ; i++) {
-        send_bt_cmd(keys[i]);
+        send_bt_byte(keys[i]);
     }
 
-    send_bt_cmd(HEADER_KEY_END);
+    send_bt_byte(HEADER_KEY_END);
 
-    set_lightbar(0, 0, 0); // "Ladies and gentlemen, we got him." (do a lightbar animation here)
+    set_lightbar(0x00, 0xff, 0x00); // "Ladies and gentlemen, we got him." (do a lightbar animation here)
 }
 
