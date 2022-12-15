547b2d331170001545ec9f539fd8e6f3f004f8bd
Aaron <aaron@exphat.com>
2022-12-14 22:47:30 -0500

    STM32Cube lib

--- Makefile
+++ Makefile
@@ -1,28 +1,28 @@
 CC=arm-none-eabi-gcc
 LD=arm-none-eabi-ld
-CFLAGS=-O0 -g -c -mcpu=cortex-m3 -mthumb
+CFLAGS=-O0 -g -mcpu=cortex-m3 -mthumb --specs=nosys.specs -T stm32.ld
 LDFLAGS=-Tstm32.ld
 
 FIRMWARE=firmware.c
 STARTUP=startup.c
 OUT=firmware.bin
-SIGNED_OUT=signed-firmware.bin
+
+#SRCS += STM32CubeF1/Drivers/CMSIS/Device/ST/STM32F1xx/Source/*.c
+SRCS=firmware.c
+SRCS += system_stm32f1xx.c
+SRCS += STM32CubeF1/Drivers/STM32F1xx_HAL_Driver/Src/*.c
+
+CFLAGS += -ISTM32CubeF1/Drivers/CMSIS/Device/ST/STM32F1xx/Include
+CFLAGS += -ISTM32CubeF1/Drivers/CMSIS/Include
+CFLAGS += -ISTM32CubeF1/Drivers/STM32F1xx_HAL_Driver/Inc
 
 $(OUT): firmware.elf
 	arm-none-eabi-objcopy -O binary firmware.elf $(OUT)
-firmware.elf: startup.o firmware.o
-	$(LD) $(LDFLAGS) -o firmware.elf startup.o firmware.o
-startup.o: $(STARTUP)
-	$(CC) $(CFLAGS) -o startup.o $(STARTUP)
-firmware.o: $(FIRMWARE)
-	$(CC) $(CFLAGS) -o firmware.o $(FIRMWARE)
-
-# $(KEY) passed in as environment variable to prevent accidental commits (-K only takes direct hex string)
-sign:
-	openssl enc -d -aes-128-ecb -nosalt -nopad -K $(KEY) -in $(OUT) -out $(SIGNED_OUT)
+firmware.elf: $(SRCS)
+	$(CC) $(CFLAGS) $^ -o $@
 
 clean:
-	rm -rf *.o *.elf $(OUT) $(SIGNED_OUT)
+	rm -rf *.o *.elf $(OUT)
 
 # arm-none-eabi-gcc -O0 -c -g -mcpu=cortex-m3 -mthumb -o firmware.o firmware.c
 # arm-none-eabi-gcc -O0 -c -g -mcpu=cortex-m3 -mthumb -o startup.o startup.c
--- firmware.c
+++ firmware.c
@@ -1,45 +1,22 @@
 #define KEY_LOCATION 0x08000d54
-#define BYTES_TO_READ 0x4
-
-// Our custom magic headers to show we're sending the firmware data
-#define HEADER_KEY_START 0x00
-#define HEADER_KEY_END 0x00
-
-#define unlock_memory   ((void (*)(void))0x08001092)
-#define set_lightbar    ((void (*)(unsigned int, unsigned int, unsigned int))0x08000c46)
-#define read_bt_byte    ((unsigned int (*)(void))0x00000000)
-#define send_bt_byte    ((void (*)(unsigned int))0x08001194)
 
 #include <string.h>
+#include "stm32f1xx.h"
+#include "system_stm32f1xx.h"
 
 unsigned int keys[4];
-unsigned int last_cmd = 0x00;
 
 void main()
 {
-    set_lightbar(0xff, 0xbe, 0x00); // Show that we've successfully run the firmware
-
-    // Ignore waiting for start header for now
-    //while (last_cmd != HEADER_KEY_START) {
-    //    last_cmd = read_bt_byte();
-    //}
-
-    set_lightbar(0xbe, 0x00, 0xff); // Acknowledge that we've received the start header
-
-    unlock_memory();
-
-    set_lightbar(0xff, 0x00, 0xbe); // Show that we've unlocked the flash and are going to read
-
-    for (int i = 0; i < BYTES_TO_READ; i++) {
+    for (int i = 0; i < 4; i++) {
         memcpy(&keys[i], (unsigned int*)(KEY_LOCATION + (i * sizeof(unsigned int))), sizeof(unsigned int));
     }
 
-    for (int i = 0; i < BYTES_TO_READ; i++) {
-        send_bt_byte(keys[i]);
+    unsigned int* test;
+
+    for (int i = 0; i < 4; i++) {
+        memcpy(test, &keys[i], sizeof(unsigned int));
     }
 
-    send_bt_byte(HEADER_KEY_END);
-
-    set_lightbar(0x00, 0xff, 0x00); // "Ladies and gentlemen, we got him." (do a lightbar animation here)
 }
 
