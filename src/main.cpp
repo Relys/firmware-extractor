fae7bf7dd59b8a4c6e25917f25b24f5afb7504ac
Nishanth Samala <hey@outlandnish.com>
2022-12-17 17:35:47 -0600

    got ota reboot working

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
-#include <FlashStorage_STM32.h>
+
+void mark_ota_reboot() {
+  HAL_FLASH_Unlock();
+  __HAL_FLASH_CLEAR_FLAG(0x35);
+  HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, 0x0800f800, 0x80a0);
+}
 
 void setup() {
-    // put OTA flag back into in for reboot
-    EEPROM.put(0x0800f800, 0x80a0);
-    EEPROM.commit();
+  mark_ota_reboot();
 }
 
 void loop() {
-
 }
